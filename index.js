f32157d004ff735038ff7e21b2b4f438dfd1fd11
will sherman <willsherman771@gmail.com>
2023-08-19 19:55:33 -0400

    added options after initial qs

--- index.js
+++ index.js
@@ -29,6 +29,48 @@ function displayMenu() {
          ]
         }
       ])
-}
+      .then((choice) => {
+        switch (choice.action) {
+          case 'View all departments':
+            // Call a function to view employees
+            viewDepartments();
+            break;
+          case 'View all roles':
+            // Call a function to add an employee
+            viewRoles();
+            break;
+          case 'View all employees':
+            // Call a function to update an employee
+            viewEmployees();
+            break;
+          case 'Add a department':
+            // Call a function to delete an employee
+            addDepartment();
+            break;
+          case 'Add a role':
+                // Call a function to delete an employee
+            addRole();
+            break;
+          case 'Add an employee':
+                // Call a function to delete an employee
+            addEmployee();
+            break;
+          case "Update an employee's role":
+                // Call a function to delete an employee
+            updateRole();
+            break;              
+          case 'Exit':
+            console.log('Goodbye!');
+            connection.end(); // Close the database connection
+            break;
+          default:
+            console.log('Invalid choice. Please try again.');
+            displayMainMenu(); // Show the main menu again
+            break;
+        }
+      });
+  }
+  
+
 
 displayMenu();
